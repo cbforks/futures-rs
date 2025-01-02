a0d4e698d9fa8119cd35cedb02473ca7db1b2eda
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2025-01-01 20:22:05 -0500

    try using core::panic in futures-core

--- futures-core/src/lib.rs
+++ futures-core/src/lib.rs
@@ -12,8 +12,8 @@
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
-#[cfg(feature = "std")]
-extern crate std;
+// #[cfg(feature = "std")]
+// extern crate std;
 
 pub mod future;
 #[doc(no_inline)]
